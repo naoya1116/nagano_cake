fd4a783b07f21df2c7bbd901b6a7a0368a43b345
naoya1116 <notice.jr@gmail.com>
2021-04-22 10:54:33 +0000

    [Add]admin_view

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,18 @@
 class Admin::ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path
+  end
+
+  private
+
+  def item_params
+   params.require(:item).permit(:genre,:name,:image,:introduction,:price,:is_active,:created_at,:updated_at)
+  end
+  
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      admin_path
+    else
+      root_path
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      new_admin_session_path
+    else
+      new_customer_session_path
+    end
+  end
+
 end
