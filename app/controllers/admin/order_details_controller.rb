65553b6fb25c408f2bd39ef517bf254823c282ab
naoya1116 <notice.jr@gmail.com>
2021-05-07 06:18:17 +0000

    [Add]admin_order

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,24 @@
 class Admin::OrderDetailsController < ApplicationController
+
+    def update
+        order_item = OrderItem.find(params[:order_item][:order_item_id])
+        order = order_item.order
+
+        #注文商品の製作ステータスが1つでも“製作中“であれば注文ステータスを“製作中“に更新
+        if params[:order_item][:makeing_status] == "製作中"
+          order.update(status: "製作中")
+        end
+        order_item.update(makeing_status: params[:order_item][:makeing_status])
+
+        #注文商品全ての製作ステータスが“製作完了“で注文ステータスを“発送準備中“に
+        order = Order.find(params[:id])
+        if  order.order_items.all? do |p|
+              p.makeing_status == "製作完了"
+            end
+            order.update(status: "発送準備中")
+            flash[:success] = "注文ステータスが「発送準備中」に更新されました"
+        end
+        redirect_to admin_order_path
+    end
+
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,34 @@
 class Admin::OrdersController < ApplicationController
+  def show
+    @order = Order.find(params[:id])
+    @order_item = @order.order_items
+
+    #商品合計
+    @total = 0
+    @order_item.each do |order_item|
+      pay = order_item.item.price * order_item.amount
+      @total += pay
+    end
+  end
+
+  def update
+    order = Order.find(params[:id])
+
+    #注文ステータスが"入金確認"になったら製作ステータスを"製作待ち"
+    if params[:order][:status] == "1"
+      order.order_items.each do |order_item|
+        order_item.makeing_status = "1"
+        order_item.update(makeing_status: order_item.makeing_status)
+      end
+    end
+    order.update(order_params)
+    redirect_to admin_order_path(order.id)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
+
 end
